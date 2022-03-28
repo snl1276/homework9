ccf0a9b64f17c6da14ae99dac64997c62110ef77
snl1276 <98837245+snl1276@users.noreply.github.com>
2022-03-29 00:05:59 +0300

    changed  main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-#import click
+import click
 from datetime import datetime
 import datetime
 
+
+@click.command()
 #@click.option('--hous', default=0, help='number of greetings')
 #@click.argument('newyear', help='You name')
 
@@ -11,9 +13,9 @@ def main():
     c = b - a
     mm, ss = divmod(c.seconds, 60)
     hh, mm = divmod(mm, 60)
-    print(c.days, 'days, ', hh, ' hours')
-    #click.echo(c.days, 'days', hh)
-
+    #print(c.days, 'days, ', hh, ' hours')
+    click.echo(f"{c.days} days")
+    click.echo(f"{hh} hours")
 if __name__ == '__main__':
     main()
     
