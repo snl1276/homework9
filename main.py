d4ccb9cbdb8ffea56294529eba13e7ed57647b61
snl1276 <98837245+snl1276@users.noreply.github.com>
2022-03-29 22:56:01 +0300

    changed  main.py

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ import datetime
 
 
 @click.command()
-#@click.option('--hous', default=0, help='number of greetings')
-#@click.argument('newyear', help='You name')
-
-def main():
+@click.argument('newyear')
+@click.option('--hours')
+def main(newyear, hours):
     a = datetime.datetime.today()
-    b = datetime.datetime(2023, 1, 1)
-    c = b - a
-    mm, ss = divmod(c.seconds, 60)
-    hh, mm = divmod(mm, 60)
+    newyear = datetime.datetime(2023, 1, 1)
+    c = newyear - a
+    minuta, secunda = divmod(c.seconds, 60)
+    hours, minuta = divmod(minuta, 60)
     #print(c.days, 'days, ', hh, ' hours')
     click.echo(f"{c.days} days")
-    click.echo(f"{hh} hours")
-if __name__ == '__main__':
-    main()
+    click.echo(f"{hours} hours")
+
     
+
+
+if __name__ == '__main__':
+    main()    
