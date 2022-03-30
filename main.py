0b3500d6b9cfc83119d4d0534e1702852b0ca307
snl1276 <98837245+snl1276@users.noreply.github.com>
2022-03-31 00:08:47 +0300

    changed  main.py

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
 import click
 from datetime import datetime
 import datetime
+import enum
+import random
 
 
-@click.command()
-@click.argument('newyear')
+class Color(enum.Enum):
+    Red = '#f00'
+    Green = '#0f0'
+    Blue = '#00f'
+    Cyan = '#0ff'
+    Magenta = '#f0f'
+    Yellow = '#ff0'
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+def toy():
+    click.echo(random.choice([Color.Red.name, Color.Green.name, Color.Blue.name, 
+    Color.Cyan.name, Color.Magenta.name, Color.Yellow.name])
+     +' ' + random.choice(['doll', 'car', 'ball', 'angel', 'bear', 'cubes']))
+
+
+@main.command()
 @click.option('--hours', is_flag=True)
-def main(newyear, hours):
+def newyear(hours):
     a = datetime.datetime.today()
     newyear = datetime.datetime(2023, 1, 1)
     c = newyear - a
     minuta, secunda = divmod(c.seconds, 60)
     hour, minuta = divmod(minuta, 60)
-    #print(c.days, 'days, ', hh, ' hours')
     if hours:
         click.echo(f"{c.days} days, {hour} hours")
     else:
         click.echo(f"{c.days} days")
+
+
+main.add_command(toy)
+main.add_command(newyear)
 
 
 if __name__ == '__main__':
