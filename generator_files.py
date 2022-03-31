ffee1dca30a38fae6af2bf80a6dde957565bc61a
snl1276 <98837245+snl1276@users.noreply.github.com>
2022-03-31 21:40:06 +0300

    new generator_files.py

---
+++ generator_files.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from random import randint 
+
+
+Path('directory').mkdir(exist_ok=True) 
+for i in range(15): 
+    Path(f'directory/'   
+         f'{randint(2020, 2022)}-'
+         f'{randint(1,12)}-'   
+         f'{randint(1, 31)}.txt' 
+    ).touch()
